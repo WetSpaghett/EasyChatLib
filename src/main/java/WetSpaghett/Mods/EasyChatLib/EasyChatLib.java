71b4a678c09e87b1ad205e77ec2f7e10479c2a22
WetSpaghett <likeabossjackie@gmail.com>
2022-07-07 18:01:25 +0100

    rename to WetSpaghett's Easy Chat Lib

--- src/main/java/WetSpaghett/Mods/EasyChatLib/EasyChatLib.java
+++ src/main/java/WetSpaghett/Mods/EasyChatLib/EasyChatLib.java
@@ -18,13 +18,11 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(EasyChatLib.MODID)
 public class EasyChatLib {
     public static final String MODID = "easychatlib";
-    public static final String NAME = "Easy Chat Lib";
-    public static final String VERSION = "v0.1.2";
+    public static final String NAME = "WetSpaghett's Easy Chat Lib";
+    public static final String VERSION = "v1.0.0";
     public static MinecraftServer server;
 
     public EasyChatLib() {
-        // Register methods
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
